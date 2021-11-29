032965293d920642163ce3b24abd6f50b2bdf690
Kazero21 <Thinker7120@hotmail.com>
2021-11-29 21:09:25 +0300

    Add MOD method

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,4 +1,13 @@
+import java.util.InputMismatchException;
 
 public class MainClass {
+	
+	public static void main(String[] args) {}
 
+	public static int MOD(int a, int m) {
+		if (!(m > 0)) throw new InputMismatchException("Wrong m value");
+		if (a > 0)	{if (!(a < m)) while (!(a < m))	a -= m;}
+		else		{while (a < 0)	a += m;}
+		return a;		
+	}
 }
