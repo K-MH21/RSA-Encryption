306b1a4d289b1f9b7d8798673f2c98e74ee5c525
Kazero21 <Thinker7120@hotmail.com>
2021-12-02 15:35:33 +0300

    Add the reading/Writing from a file functionality in encryp() method + some general orginzations

--- src/MainClass.java
+++ src/MainClass.java
@@ -9,31 +9,27 @@ public class MainClass {
     final static ArrayList<String> Alphabet_Values = new ArrayList<>();
     static String dmessage="";
     static String emessage="";
-    static String alphabet;
-
+    static String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.?!,;:-()[]{}'\" \n";
+    static Scanner kb = new Scanner(System.in);
+    
     public static void main(String[] args) throws IOException {
         //Preparing Alphabet_Values and alphabet
-        {
-            String UC = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z".replaceAll(" ", "");
-            String LC = "a b c d e f g h i j k l m n o p q r s t u v w x y z".replaceAll(" ", "");
-            String N = "0123456789";
-            String special = ".?!,;:-()[]{}'\" \n";
-            alphabet = UC + LC + N + special;
-            for (Integer i = 0; i < alphabet.length(); i++) {
+            for (Integer i = 0; i < alphabet.length(); i++) 
                 if (i < 10)	Alphabet_Values.add("0" + i.toString());
                 else		Alphabet_Values.add(i.toString());
-            }
-        }
-        //Main menu
+            
+            //Main menu
         while (true) {
             System.out.println("What do you want to do?"
                     + "\n(1) Encrypt a message"
                     + "\n(2) Decrypt a massage"
                     + "\n(3) Exit the System");
-            switch (new Scanner(System.in).nextInt()) {
+            switch (kb.nextInt()) {
                 case 1: encrypt();				break;
                 case 2: decrypt();				break;
-                case 3: System.exit(0);
+                case 3:
+                	System.out.println("System has been terminated");
+                	System.exit(0);
             }
         }
     }
@@ -45,53 +41,56 @@ public class MainClass {
     }
 
     /*
-     * Encryption method, Will take a .txt file as an input
-     * and will produce a .rsa file as an output.
+     * Encryption method will take a .txt file as an input
+     * and will produce an .rsa file as an output.
      */
     private static void encrypt() throws IOException {
-        //	Scanner read;
-        //	try {read = new Scanner(new File("message.txt"));}
-        //	catch (FileNotFoundException e) {System.out.println("File is missing"); return;}
-        long e = 5; //read.nextLong(); 	TODO Throw an exception if e or n is wrong
-        long n = 17*7; //read.nextLong();  TODO Need to check which value comes first in the message.txt
+    	//Getting the message
+       	Scanner read;
+       	try {read = new Scanner(new File("message.txt"));}
+       	catch (FileNotFoundException e) {System.out.println("File is missing"); return;}
+       	
+       	//Getting the values
+       	String dmessage = "";
+        long e = read.nextLong(); 
+        long n = read.nextLong();  //TODO Need to check which value comes first in the message.txt
+       	while (read.hasNextLine()) dmessage += read.nextLine();
+
+       	//Finding the blockLength
         Integer blockLength = 0;
-        while (n > blockLength) {
-            int power = 0;
-            blockLength = (int) (blockLength + (alphabet.length() - 1) * Math.pow(10, power));
-            power += 2;
-        }
+        for (int power = 0; n > blockLength + (alphabet.length() - 1) * Math.pow(10, power); power += 2)
+            blockLength += (int) ((alphabet.length() - 1) * Math.pow(10, power));
         blockLength = blockLength.toString().length()/2;
-        String Emessage = "ST 5 ST";
-         emessage = "";
-        String temp = "";
-        //	while (read.hasNextLine()) message += read.nextLine();
-        for (int i = 0, j = 1; i<Emessage.length(); i++, j++) {
-            int s = 0;
-            while (Emessage.charAt(i) != alphabet.charAt(s))	s++;
-            temp += Alphabet_Values.get(s);
-            if (j == blockLength) {
-                System.out.println(Long.valueOf(temp));
-                j = 0;
-                emessage += MOD((long) Math.pow(Long.valueOf(temp), e), n);
-
-
-
-                temp = "";
-            }
-        }
         
-        // TODO create an encrypted file
-/*
-FileWriter fileWriter = new FileWriter("emessage.rsa");
+        //Encrypting the message
+        String emessage = "";
+		{
+			String temp;
+			int i, j;
+			for (temp = "", i = 0, j = 1; i < dmessage.length(); i++, j++) {
+				int s;
+				for (s = 0; dmessage.charAt(i) != alphabet.charAt(s); s++);
+				temp += Alphabet_Values.get(s);
+				if (j == blockLength) {
+					j = 0;
+					emessage += MOD((long) Math.pow(Long.valueOf(temp), e), n);
+					temp = "";
+				}
+			}
+		}
+        
+		//Creating the rsa file and writing on it
+		FileWriter fileWriter = new FileWriter("emessage.rsa");
         fileWriter.write(emessage);
-        fileWriter.close();*/
-
-        System.out.println(emessage);
+        fileWriter.close();
+        
+        //End of the method
+        System.out.println("The message has been encrypted successfully");
     }
 
     private static void decrypt() throws FileNotFoundException {
         
-        Scanner kb = new Scanner(System.in);
+        
         System.out.print("Enter d's value: ");
         long d = kb.nextLong();
         System.out.print("Enter n's value: ");
